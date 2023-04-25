69084a85015ed2039b157e25742c411bd22b453f
Jatin Kharbanda <jatinkharbanda33@gmail.com>
2023-04-25 23:13:35 +0530

    Add files via commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import '../models/categories.dart';
-import './models/Categorymealscreen.dart';
+import 'Screens/categoriesscreen.dart';
+import 'Screens/Categorymealscreen.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ lib/models/MealItems.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Stack(
+        children: [
+          
+        ],
+        
+      ),
+      
+      
+    );
+  }
+}
